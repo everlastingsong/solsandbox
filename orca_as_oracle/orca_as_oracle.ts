0e0345ac8c8fcb83330fec6d5ad8bd8f4c0a85b7
everlastingsong <98769788+everlastingsong@users.noreply.github.com>
2022-03-03 14:58:57 +0900

    Update orca_as_oracle.ts

--- orca_as_oracle/orca_as_oracle.ts
+++ orca_as_oracle/orca_as_oracle.ts
@@ -8,21 +8,22 @@ const commitment = 'confirmed';
 const connection = new Connection(RPC_ENDPOINT_URL, commitment);
 
 async function main() {
+    const fee = 0.003; // 0.3%
+
     const orca = getOrca(connection);
     const sol_usdc_pool = orca.getPool(OrcaPoolConfig.SOL_USDC);
     const orca_sol_pool = orca.getPool(OrcaPoolConfig.ORCA_SOL);
 
     const sol_amount = new Decimal(1);
     const usdc_sol_quote = await sol_usdc_pool.getQuote(sol_usdc_pool.getTokenA(), sol_amount);
-    const usdc_per_sol = usdc_sol_quote.getExpectedOutputAmount(); // not Minimum
+    const usdc_per_sol = usdc_sol_quote.getExpectedOutputAmount().toNumber() / (1-fee); // not Minimum
 
     const orca_amount = new Decimal(1);
     const sol_orca_quote = await orca_sol_pool.getQuote(orca_sol_pool.getTokenA(), orca_amount);
-    const sol_per_orca = sol_orca_quote.getExpectedOutputAmount(); // not Minimum
+    const sol_per_orca = sol_orca_quote.getExpectedOutputAmount().toNumber() / (1-fee); // not Minimum
 
-    const fee = 0.003; // 0.3%
-    console.log("USDC/SOL price: ", usdc_per_sol.toNumber()/(1-fee));
-    console.log("USDC/ORCA price", sol_per_orca.toNumber()/(1-fee) * usdc_per_sol.toNumber()/(1-fee));
+    console.log("USDC/SOL price: ", usdc_per_sol);
+    console.log("USDC/ORCA price", sol_per_orca * usdc_per_sol);
 }
 
 main();
