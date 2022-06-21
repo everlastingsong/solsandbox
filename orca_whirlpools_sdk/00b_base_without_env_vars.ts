f47a2ccbd04c5254c24d4d1475385bfe8f16cc75
yugure <98769788+everlastingsong@users.noreply.github.com>
2022-06-21 11:46:49 +0900

    Update 00b_base_without_env_vars.ts

--- orca_whirlpools_sdk/00b_base_without_env_vars.ts
+++ orca_whirlpools_sdk/00b_base_without_env_vars.ts
@@ -1,17 +1,16 @@
 import { WhirlpoolContext, AccountFetcher, ORCA_WHIRLPOOL_PROGRAM_ID, buildWhirlpoolClient } from "@orca-so/whirlpools-sdk";
-import { Provider } from "@project-serum/anchor";
-const { Wallet } = require("@project-serum/anchor"); // v0.20.1 bug, import is not possible
+const { Wallet } = require("@project-serum/anchor"); // v0.20.1 bug, import for Wallet is not possible
 import { Keypair, Connection } from "@solana/web3.js";
 
 const RPC_ENDPOINT_URL="https://ssc-dao.genesysgo.net"
 const COMMITMENT = "confirmed";
 
 const connection = new Connection(RPC_ENDPOINT_URL, COMMITMENT);
-const keypair = Keypair.generate(); // Dummy
-const provider = new Provider(connection, new Wallet(keypair), Provider.defaultOptions());
-console.log("connection endpoint", provider.connection.rpcEndpoint);
-console.log("wallet", provider.wallet.publicKey.toBase58());
+const wallet = new Wallet(Keypair.generate()) // dummy
 
-const ctx = WhirlpoolContext.withProvider(provider, ORCA_WHIRLPOOL_PROGRAM_ID);
+const ctx = WhirlpoolContext.from(connection, wallet, ORCA_WHIRLPOOL_PROGRAM_ID);
 const fetcher = new AccountFetcher(ctx.connection);
 const client = buildWhirlpoolClient(ctx, fetcher);
+
+console.log("connection endpoint", ctx.connection.rpcEndpoint);
+console.log("wallet", ctx.wallet.publicKey.toBase58());
