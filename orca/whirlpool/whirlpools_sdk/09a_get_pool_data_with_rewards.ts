ddfee20aae8adf86ee43ed76cb3025ec51e9fd6b
yugure <98769788+everlastingsong@users.noreply.github.com>
2023-03-10 20:57:06 +0900

    Update 09a_get_pool_data_with_rewards.ts

--- orca/whirlpool/whirlpools_sdk/09a_get_pool_data_with_rewards.ts
+++ orca/whirlpool/whirlpools_sdk/09a_get_pool_data_with_rewards.ts
@@ -1,52 +1,55 @@
 import { PublicKey } from "@solana/web3.js";
 import {
-    WhirlpoolContext, AccountFetcher, ORCA_WHIRLPOOL_PROGRAM_ID, buildWhirlpoolClient,
-    PDAUtil, ORCA_WHIRLPOOLS_CONFIG, PriceMath, PoolUtil
+    WhirlpoolContext, ORCA_WHIRLPOOL_PROGRAM_ID, buildWhirlpoolClient,
+    PriceMath, PoolUtil
 } from "@orca-so/whirlpools-sdk";
-import { Provider, BN } from "@project-serum/anchor";
+import { AnchorProvider, BN } from "@project-serum/anchor";
 import { MintLayout } from "@solana/spl-token";
 import Decimal from "decimal.js";
 
 // THIS SCRIPT REQUIRES ENVIRON VARS!!!
-// bash$ export ANCHOR_PROVIDER_URL=https://api.devnet.solana.com
+// bash$ export ANCHOR_PROVIDER_URL=https://api.mainnet-beta.solana.com
 // bash$ export ANCHOR_WALLET=~/.config/solana/id.json
 // bash$ ts-node this_script.ts
 
-interface TokenDefinition {
-  symbol: string,
-  mint: PublicKey,
-  decimals: number,
-}
-
-const provider = Provider.env();
+const provider = AnchorProvider.env();
 console.log("connection endpoint", provider.connection.rpcEndpoint);
 console.log("wallet", provider.wallet.publicKey.toBase58());
 
 async function main() {
     const ctx = WhirlpoolContext.withProvider(provider, ORCA_WHIRLPOOL_PROGRAM_ID);
-    const fetcher = new AccountFetcher(ctx.connection);
-    const client = buildWhirlpoolClient(ctx, fetcher);
+    const client = buildWhirlpoolClient(ctx);
 
-    const config_pubkey = new PublicKey("GLGWDKtwhKm5o353KuUx7Z6iSnVVjdSRhAejvH8urX8F");
-    const SOL : TokenDefinition = {symbol: "SOL", mint: new PublicKey("So11111111111111111111111111111111111111112"), decimals: 9};
-    const USDC: TokenDefinition = {symbol: "devUSDC", mint: new PublicKey("FMwbjM1stnTzi74LV4cS937jeSUds7mZDgcdgnJ1yBDw"), decimals: 6};
-    const tick_spacing = 64;
-  
-    const whirlpool_pubkey = PDAUtil.getWhirlpool(
-        ORCA_WHIRLPOOL_PROGRAM_ID,
-        config_pubkey,
-        SOL.mint, USDC.mint, tick_spacing).publicKey;
+    const SOL_mSOL_1_WHIRLPOOL = new PublicKey("HQcY5n2zP6rW74fyFEhWeBd3LnJpBcZechkvJpmdb8cx");
+    const SOL_USDC_8_WHIRLPOOL = new PublicKey("7qbRF6YsyGuLUVs6Y1q64bdVrfe4ZcUUz1JRdoVNUJnm");
+    const SOL_USDC_64_WHIRLPOOL = new PublicKey("HJPjoWUrhoZzkNfRpHuieeFk9WcZWjwy6PBjZ81ngndJ");
+    const CAVE_USDC_128_WHIRLPOOL = new PublicKey("7mMFeKDeJFcefhTe3ZTR11wUS3CUDexsJ61crJ8oekdL");
+    const STSOL_USDC_64_WHIRLPOOL = new PublicKey("AXtdSZ2mpagmtM5aipN5kV9CyGBA8dxhSBnqMRp7UpdN");
+
+    const whirlpool_pubkey = STSOL_USDC_64_WHIRLPOOL;
     console.log("whirlpool_key", whirlpool_pubkey.toBase58());
+
     const whirlpool = await client.getPool(whirlpool_pubkey);
     const whirlpool_data = whirlpool.getData()
 
     // WhirlpoolData type members: https://orca-so.github.io/whirlpools/modules.html#WhirlpoolData
+    console.log("rewardLastUpdatedTimestamp", whirlpool_data.rewardLastUpdatedTimestamp.toString())
     console.log("liquidity", whirlpool_data.liquidity.toString());
     console.log("sqrtPrice", whirlpool_data.sqrtPrice.toString());
     console.log("tickCurrentIndex", whirlpool_data.tickCurrentIndex);
-    console.log("price (from tickCurrentIndex)", PriceMath.tickIndexToPrice(whirlpool_data.tickCurrentIndex, SOL.decimals, USDC.decimals));
+    console.log("price (from tickCurrentIndex)", PriceMath.tickIndexToPrice(whirlpool_data.tickCurrentIndex, whirlpool.getTokenAInfo().decimals, whirlpool.getTokenBInfo().decimals));
+    console.log("price (from sqrtPrice)", PriceMath.sqrtPriceX64ToPrice(whirlpool_data.sqrtPrice, whirlpool.getTokenAInfo().decimals, whirlpool.getTokenBInfo().decimals));
+    console.log("tokenMintA", whirlpool_data.tokenMintA.toBase58());
+    console.log("tokenMintB", whirlpool_data.tokenMintB.toBase58());
     console.log("tokenVaultA", whirlpool_data.tokenVaultA.toBase58());
     console.log("tokenVaultB", whirlpool_data.tokenVaultB.toBase58());
+    console.log("feeGrowthGlobalA", whirlpool_data.feeGrowthGlobalA.toString());
+    console.log("feeGrowthGlobalB", whirlpool_data.feeGrowthGlobalB.toString());
+    console.log("protocolFeeOwedA", whirlpool_data.protocolFeeOwedA.toString());
+    console.log("protocolFeeOwedB", whirlpool_data.protocolFeeOwedB.toString());
+    console.log("feeRate", whirlpool_data.feeRate.toString(), "/1000000");
+    console.log("protocolFeeRate", whirlpool_data.protocolFeeRate.toString(), "/10000");
+    console.log("rewardLastUpdatedTimestamp", whirlpool_data.rewardLastUpdatedTimestamp.toString())
     for (let i=0; i<3; i++) {
       if ( PoolUtil.isRewardInitialized(whirlpool_data.rewardInfos[i]) ) {
         const mint_account_info = await ctx.connection.getAccountInfo(whirlpool_data.rewardInfos[i].mint);
@@ -59,17 +62,21 @@ async function main() {
         console.log(`reward[${i}] growthGlobal`, whirlpool_data.rewardInfos[i].growthGlobalX64.toString());
         console.log(`reward[${i}] * emissionsPerSecond`, whirlpool_data.rewardInfos[i].emissionsPerSecondX64.toString());
 
-        const emissionsPerWeek = new Decimal(whirlpool_data.rewardInfos[i].emissionsPerSecondX64
-          .shrn(64)
-          .muln(60*60*24*7).toString())
+        const emissionsPerWeek = new Decimal(whirlpool_data.rewardInfos[i].emissionsPerSecondX64.toString())
+          .div(new Decimal(2).pow(64))
+          .mul(60*60*24*7)
           .div(new Decimal(10**decimals))
           .ceil();
 
-        //console.log(`reward[${i}] * emissionsPerWeek`, whirlpool_data.rewardInfos[i].emissionsPerSecondX64.shrn(64).muln(60*60*24*7).div(new BN(10**decimals)).toString());
         console.log(`reward[${i}] * emissionsPerWeek`, emissionsPerWeek.toString());
       }
       else {
         console.log(`reward[${i}] UNINITIALIZED`);
+        console.log(`reward[${i}] mint`, whirlpool_data.rewardInfos[i].mint.toBase58());
+        console.log(`reward[${i}] vault`, whirlpool_data.rewardInfos[i].vault.toBase58());
+        console.log(`reward[${i}] authority`, whirlpool_data.rewardInfos[i].authority.toBase58());
+        console.log(`reward[${i}] growthGlobal`, whirlpool_data.rewardInfos[i].growthGlobalX64.toString());
+        console.log(`reward[${i}] * emissionsPerSecond`, whirlpool_data.rewardInfos[i].emissionsPerSecondX64.toString());
       }
     }
 }
