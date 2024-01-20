40b6972e3af3d92b136affcad77833f6d4e17b78
yugure <98769788+everlastingsong@users.noreply.github.com>
2024-01-20 13:21:12 +0900

    Update 00a_base.ts

--- orca/whirlpool/whirlpools_sdk/00a_base.ts
+++ orca/whirlpool/whirlpools_sdk/00a_base.ts
@@ -1,13 +1,18 @@
-import { WhirlpoolContext, AccountFetcher, ORCA_WHIRLPOOL_PROGRAM_ID, buildWhirlpoolClient } from "@orca-so/whirlpools-sdk";
-import { Provider } from "@project-serum/anchor"; // import AnchorProvider.env() if the version of Anchor >= 0.24.0
+import { WhirlpoolContext, ORCA_WHIRLPOOL_PROGRAM_ID, buildWhirlpoolClient } from "@orca-so/whirlpools-sdk";
+import { AnchorProvider } from "@coral-xyz/anchor";
 
-// bash$ export ANCHOR_PROVIDER_URL=https://ssc-dao.genesysgo.net
-// bash$ export ANCHOR_WALLET=~/.config/solana/id.json
+// bash$ export ANCHOR_PROVIDER_URL=<YOUR RPC ENDPOINT>
+// bash$ export ANCHOR_WALLET=<YOUR WALLET JSON FILEPATH>
 // bash$ ts-node this_script.ts
-const provider = Provider.env(); // use AnchorProvider.env() if the version of Anchor >= 0.24.0
-console.log("connection endpoint", provider.connection.rpcEndpoint);
-console.log("wallet", provider.wallet.publicKey.toBase58());
 
-const ctx = WhirlpoolContext.from(provider.connection, provider.wallet, ORCA_WHIRLPOOL_PROGRAM_ID);
-const fetcher = new AccountFetcher(ctx.connection);
-const client = buildWhirlpoolClient(ctx, fetcher);
+async function main() {
+  const provider = AnchorProvider.env();
+  console.log("connection endpoint", provider.connection.rpcEndpoint);
+  console.log("wallet", provider.wallet.publicKey.toBase58());
+
+  const ctx = WhirlpoolContext.from(provider.connection, provider.wallet, ORCA_WHIRLPOOL_PROGRAM_ID);
+  const fetcher = ctx.fetcher;
+  const client = buildWhirlpoolClient(ctx);
+}
+
+main();
